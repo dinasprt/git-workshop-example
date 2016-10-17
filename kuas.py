217a0b87c4eef955fde731b32dc4b4ebcde3a257
dinasprt <1104108124@gm.kuas.edu.tw>
2016-10-17 20:42:34 +0800

    456

--- kuas.py
+++ kuas.py
@@ -1,6 +1,5 @@
 #! /usr/bin/python3
 
-"""
 # Here for person A
 
 import requests
@@ -21,9 +20,7 @@ def getlist():
 		News.append( [ t.text, t.attrib[ 'href' ] ] )
 
 	return News
-"""
 
-"""
 # Here for person B
 
 def getcontent( url ):
